93363276c291198c6e1459d08df4f0f2ea43a5f5
Carlos Andres Restrepo Marin <cr461204@gmail.com>
2021-12-09 10:58:55 -0500

    back prod url added

--- src/redux/api/mainAPI.js
+++ src/redux/api/mainAPI.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const mainAPI = createApi({
   reducerPath: "mainAPI",
   baseQuery: fetchBaseQuery(
-    { baseUrl: "http://127.0.0.1:5000/"
+    { baseUrl: "https://estimate-price-property.herokuapp.com/"
     }),
   endpoints: (builder) => ({
    
